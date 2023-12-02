f753194af2574d38fe106477d71b0b8ed334dd67
“Behnekoogol” <behnekoogol@gmail.com>
2023-12-02 19:04:44 +0330

    update settings.py

--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'treebeard',
+    'django_filters',
+
+
+    'catalog',
+    'media',
+    'inventory',
 ]
 
 MIDDLEWARE = [
@@ -116,6 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = Path.joinpath(BASE_DIR, 'media/media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
